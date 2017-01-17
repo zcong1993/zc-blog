ba8befde5e2b580de45cfbe7d37cf1136184a419
zcong1993 <1005997919@qq.com>
2017-01-17 18:05:40 +0800

    update schema

--- schemas/index.js
+++ schemas/index.js
@@ -1,5 +1,25 @@
 const mongoose = require('mongoose')
 
-var Schema = mongoose.Schema
-var ObjectId = Schema.ObjectId
+const Schema = mongoose.Schema
+const ObjectId = Schema.ObjectId
 
+const categoriesSchema = new Schema({
+  type: {
+    type: String,
+    require: [true, 'category type is required!']
+  },
+  name: {
+    type: String,
+    require: [true, 'category name is required!']
+  }
+})
+
+const postSchema = new Schema({
+  _id: ObjectId,
+  title: String,
+  author: String,
+  categories: [categoriesSchema],
+  content: String
+})
+
+exports.Post = mongoose.model('Post'. postSchema)
--- utils/mock.js
+++ utils/mock.js
@@ -9,6 +9,10 @@ const postSampleData = {
     {
       type: 'pure',
       name: 'Pure'
+    },
+    {
+      type: 'js',
+      name: 'Js'
     }
   ],
   content: 'Yesterday at CSSConf, we launched Pure – a new CSS library. Phew! Here are the <a href="https://speakerdeck.com/tilomitra/pure-bliss">slides from the presentation</a>. Although it looks pretty minimalist, we’ve been working on Pure for several months. After many iterations, we have released Pure as a set of small, responsive, CSS modules that you can use in every web project.'
